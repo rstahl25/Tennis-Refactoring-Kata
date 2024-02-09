4b2aa4b65d7083ba192c9e25c5ae31a62c74daa9
rstahl25 <135140579+rstahl25@users.noreply.github.com>
2024-02-09 19:25:28 +0000

    Add comments to TennisGame1.js for clarification

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,3 +1,4 @@
+//Sets up player1 and player2 objects with score and name attributes
 var TennisGame1 = function(player1Name, player2Name) {
     this.m_score1 = 0;
     this.m_score2 = 0;
@@ -5,6 +6,7 @@ var TennisGame1 = function(player1Name, player2Name) {
     this.player2Name = player2Name;
 };
 
+//Awards a point to whichever player won a volley
 TennisGame1.prototype.wonPoint = function(playerName) {
     if (playerName === "player1")
         this.m_score1 += 1;
@@ -12,9 +14,11 @@ TennisGame1.prototype.wonPoint = function(playerName) {
         this.m_score2 += 1;
 };
 
+//Assigns tennis-terminology equivalent to the current score
 TennisGame1.prototype.getScore = function() {
     var score = "";
     var tempScore = 0;
+    //if scores are equal, assign proper term depending on scores
     if (this.m_score1 === this.m_score2) {
         switch (this.m_score1) {
             case 0:
@@ -30,13 +34,32 @@ TennisGame1.prototype.getScore = function() {
                 score = "Deuce";
                 break;
         }
+    //If scores are not equal, determine which player has advantag/victory
+
+        //Checks to see that both players have greater than or equal to 4 points and their scores are not currently equal
     } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
+
+        //Checks difference between scores to determine advantage
         var minusResult = this.m_score1 - this.m_score2;
-        if (minusResult === 1) score = "Advantage player1";
+
+        //Positive difference --> player 1 has advantage
+        if (minusResult === 1) score = "Advantage player1"; 
+        
+        //Negative difference --> player 2 has advantage
         else if (minusResult === -1) score = "Advantage player2";
+
+        //If score is greater than 4 and difference is positive and greater than or equal to 2, player 1 wins set
         else if (minusResult >= 2) score = "Win for player1";
-        else score = "Win for player2";
+        
+        //If score is greater than 4 and difference is negative and less than or equal to -2, player 2 wins set
+        else score = "Win for player2";      
+        
+        //If scores are not equal, and if both players have less than 4 points this set, 
+        //  assign respective terminology to proper player
     } else {
+
+        //Pass 1: assign player1 score to 'tempScore' and assign proper term to player1 score string
+        //Pass 2: assign player2 score to 'tempScore' and assign proper term to player2 score string
         for (var i = 1; i < 3; i++) {
             if (i === 1) tempScore = this.m_score1;
             else {
